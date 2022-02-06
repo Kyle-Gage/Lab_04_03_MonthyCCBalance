9eb0be2af64d01091cfdb71bce2201afd713d7b7
Kyle-Gage <gageka@mail.uc.edu>
2022-02-06 17:43:52 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+	/*
+	A program that starts with a credit card balance of $5000 and then calculates the interest at a rate of 17%.
+	Assume the user does not make any payments.
+	Display the interest due after one month and again after two months.
+	 */
+
+    double creditCardBalance = 5000;
+    double interest = creditCardBalance * 0.17;
+    double newCreditBalance = creditCardBalance + interest;
+
+    
+    }
+}
